6d52e3c7dd99f6e07c8b20c87dc8c692eeded15f
ysd-marrrr <41146529+ysd-marrrr@users.noreply.github.com>
2020-04-17 20:14:50 +0900

    SASS使用のサンプルを追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,12 @@
+const path = require('path');
 module.exports = {
   stories: ['../components/**/*.stories.js'],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../src/scss'),
+    });
+    return config;
+  },
 };
--- components/sample/index.stories.js
+++ components/sample/index.stories.js
@@ -1,5 +1,10 @@
+import '../../src/scss/sample.scss';
 import { storiesOf } from '@storybook/html';
-import defaultTemplate from './default.html'
+import defaultTemplate from './default.html';
+import dialogSample from './dialog.html';
 
 storiesOf('Common/Sample', module)
 .add('default', () => defaultTemplate);
+
+storiesOf('Common/Sample', module)
+.add('dialog', () => dialogSample);
